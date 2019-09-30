7ce4784efddfa858848dcfd3d66f72c207d92672
Nathan Langridge <nathanlangridge@hotmail.com>
2019-09-30 21:38:26 +0100

    MVP

---
+++ whodunnit_homework.js
@@ -0,0 +1,224 @@
+#### Episode 1
+
+```js
+const scenario = {
+  murderer: 'Miss Scarlet',
+  room: 'Library',
+  weapon: 'Rope'
+};
+
+const declareMurderer = function() {
+  return `The murderer is ${scenario.murderer}.`;
+}
+
+const verdict = declareMurderer();
+console.log(verdict);
+```
+
+    Answer: The murderer is Miss Scarlet as const is used consistently.
+
+#### Episode 2
+
+```js
+const murderer = 'Professor Plum';
+
+const changeMurderer = function() {
+  murderer = 'Mrs. Peacock';
+}
+
+const declareMurderer = function() {
+  return `The murderer is ${murderer}.`;
+}
+
+changeMurderer();
+const verdict = declareMurderer();
+console.log(verdict);
+```
+
+    Answer: Error as already assigned muderer as constant.
+
+#### Episode 3
+
+```js
+let murderer = 'Professor Plum';
+
+const declareMurderer = function() {
+  let murderer = 'Mrs. Peacock';
+  return `The murderer is ${murderer}.`;
+}
+
+const firstVerdict = declareMurderer();
+console.log('First Verdict: ', firstVerdict);
+
+const secondVerdict = `The murderer is ${murderer}.`;
+console.log('Second Verdict: ', secondVerdict);
+```
+
+    Answer: First Verdict - The murderer is Mrs. Peacock as uses the declareMurderer function.
+            Second Verdict - The murderer is Professor Plum as only uses a variable.
+
+#### Episode 4
+
+```js
+let suspectOne = 'Miss Scarlet';
+let suspectTwo = 'Professor Plum';
+let suspectThree = 'Mrs. Peacock';
+
+const declareAllSuspects = function() {
+  let suspectThree = 'Colonel Mustard';
+  return `The suspects are ${suspectOne}, ${suspectTwo}, ${suspectThree}.`;
+}
+
+const suspects = declareAllSuspects();
+console.log(suspects);
+console.log(`Suspect three is ${suspectThree}.`);
+```
+
+    Answer: The suspects are Miss Scarlet, Professor Plum and Colonel Mustard
+            as let is used to change the contents.
+            Suspect three is Mrs. Peacock as does not use declareAllSuspects function.
+
+#### Episode 5
+
+```js
+const scenario = {
+  murderer: 'Miss Scarlet',
+  room: 'Kitchen',
+  weapon: 'Candle Stick'
+};
+
+const changeWeapon = function(newWeapon) {
+  scenario.weapon = newWeapon;
+}
+
+const declareWeapon = function() {
+  return `The weapon is the ${scenario.weapon}.`;
+}
+
+changeWeapon('Revolver');
+const verdict = declareWeapon();
+console.log(verdict);
+```
+
+    Answer: The weapon is the 'Revolver' as using declareWeapon function.
+
+#### Episode 6
+
+```js
+let murderer = 'Colonel Mustard';
+
+const changeMurderer = function() {
+  murderer = 'Mr. Green';
+
+  const plotTwist = function() {
+    murderer = 'Mrs. White';
+  }
+
+  plotTwist();
+}
+
+const declareMurderer = function () {
+  return `The murderer is ${murderer}.`;
+}
+
+changeMurderer();
+const verdict = declareMurderer();
+console.log(verdict);
+```
+
+    Answer: The murderer is Mrs. White as the last function to complete is plotTwist.
+
+#### Episode 7
+
+```js
+let murderer = 'Professor Plum';
+
+const changeMurderer = function() {
+  murderer = 'Mr. Green';
+
+  const plotTwist = function() {
+    let murderer = 'Colonel Mustard';
+
+    const unexpectedOutcome = function() {
+      murderer = 'Miss Scarlet';
+    }
+
+    unexpectedOutcome();
+  }
+
+  plotTwist();
+}
+
+const declareMurderer = function() {
+  return `The murderer is ${murderer}.`;
+}
+
+changeMurderer();
+const verdict = declareMurderer();
+console.log(verdict);
+```
+
+    Answer: The murderer is Mr. Green as declareMurderer function is defined
+            within changeMurderer function.
+
+#### Episode 8
+
+```js
+const scenario = {
+  murderer: 'Mrs. Peacock',
+  room: 'Conservatory',
+  weapon: 'Lead Pipe'
+};
+
+const changeScenario = function() {
+  scenario.murderer = 'Mrs. Peacock';
+  scenario.room = 'Dining Room';
+
+  const plotTwist = function(room) {
+    if (scenario.room === room) {
+      scenario.murderer = 'Colonel Mustard';
+    }
+
+    const unexpectedOutcome = function(murderer) {
+      if (scenario.murderer === murderer) {
+        scenario.weapon = 'Candle Stick';
+      }
+    }
+
+    unexpectedOutcome('Colonel Mustard');
+  }
+
+  plotTwist('Dining Room');
+}
+
+const declareWeapon = function() {
+  return `The weapon is ${scenario.weapon}.`
+}
+
+changeScenario();
+const verdict = declareWeapon();
+console.log(verdict);
+```
+
+    Answer: The weapon is the Candle Stick as was originally Mrs. Peacock in Dining Room.
+            Then because Dining Room === Dining Room, changes to Colonel Mustard.
+            Then because Colonel Mustard === Colonel Mustard, changes to Candle Stick.
+
+#### Episode 9
+
+```js
+let murderer = 'Professor Plum';
+
+if (murderer === 'Professor Plum') {
+  let murderer = 'Mrs. Peacock';
+}
+
+const declareMurderer = function() {
+  return `The murderer is ${murderer}.`;
+}
+
+const verdict = declareMurderer();
+console.log(verdict);
+```
+
+    Answer: The murderer is Professor Plum due to Mrs. Peacock only existing for a single line.
